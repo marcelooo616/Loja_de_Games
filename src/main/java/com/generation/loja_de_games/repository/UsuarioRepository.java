8c2c45a8d8487fdc46bd153c37dcdc2863a8ffef
marcelooo616 <marcelooo2014@hotmail.com>
2022-07-12 10:13:11 -0300

    adicionando camada de segurança

---
+++ src/main/java/com/generation/loja_de_games/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.generation.loja_de_games.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.loja_de_games.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
+
+	public Optional<Usuario> findByUsuario(String  usuario);
+	
+}
